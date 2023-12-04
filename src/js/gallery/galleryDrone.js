23fadbb35370e25b5b5d6ecac7f1a98a5b13147b
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-12-04 10:26:45 +0200

    remove /

--- src/js/gallery/galleryDrone.js
+++ src/js/gallery/galleryDrone.js
@@ -1,27 +1,27 @@
 export const galleryItemsDrone = [
     {
         original:
-            '/images/imgSimpleBox/drone1_1280.png',
+            'images/imgSimpleBox/drone1_1280.png',
         description: 'Drone mountain field',
     },
     {
         original:
-            '/images/imgSimpleBox/drone2_1280.png',
+            'images/imgSimpleBox/drone2_1280.png',
         description: 'Gerl\'s road drone',
     },
     {
         original:
-            '/images/imgSimpleBox/drone3_1280.png',
+            'images/imgSimpleBox/drone3_1280.png',
         description: 'Drone on the mountain',
     },
     {
         original:
-            '/images/imgSimpleBox/drone4_1280.png',
+            'images/imgSimpleBox/drone4_1280.png',
         description: 'Drones',
     },
     {
         original:
-            '/images/imgSimpleBox/drone5_1280.png',
+            'images/imgSimpleBox/drone5_1280.png',
         description: 'Drone launch'
     },
 ];
--- src/js/gallery/galleryMountains.js
+++ src/js/gallery/galleryMountains.js
@@ -1,27 +1,27 @@
 export const galleryItems = [
   {
     original:
-      '/images/imgSimpleBox/mountains1_1280.png',
+      'images/imgSimpleBox/mountains1_1280.png',
     description: 'Montain sun',
   },
   {
     original:
-      '/images/imgSimpleBox/mountains2_1280.png',
+      'images/imgSimpleBox/mountains2_1280.png',
     description: 'Field mountain forest',
   },
   {
     original:
-      '/images/imgSimpleBox/mountains3_1280.png',
+      'images/imgSimpleBox/mountains3_1280.png',
     description: 'Mountain liner village',
   },
   {
     original:
-      '/images/imgSimpleBox/mountains4_1280.png',
+      'images/imgSimpleBox/mountains4_1280.png',
     description: 'Mountains forest road',
   },
   {
     original:
-      '/images/imgSimpleBox/mountains5_1280.png',
+      'images/imgSimpleBox/mountains5_1280.png',
     description: 'Mountain flowers sunset sky'
   },
 ];
