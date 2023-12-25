9433f3423a07c99d2a1a5adfb005dcd6e3d72ffd
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-12-25 10:19:14 +0200

    111

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+// const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+
+// const filesDir = './src/partials';
+// const files = fs.readdirSync(filesDir);
+
+// const htmlPlugins = files.map(file => {
+//     return new HtmlWebpackPlugin({
+//         template: `./src/partials/${file}`,
+//         filename: file.replace('.html', '.ejs'),
+//         inject: 'body',
+//         content: file.replace('.html', ''),
+//     })
+// })
 
 module.exports = {
     entry: {
@@ -73,8 +86,11 @@ module.exports = {
         ],
     },
     plugins: [
+        // ...htmlPlugins,
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
         }),
         new MiniCssExtractPlugin({
             filename: 'styles.css',
@@ -95,128 +111,3 @@ module.exports = {
         }),
     ],
 };
-
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// module.exports = {
-//     entry: {
-//         main: path.resolve(__dirname, './src/index.js'),
-//     },
-//     output: {
-//         filename: 'main.js',
-//         path: path.resolve(__dirname, 'dist'),
-//     },
-//     mode: 'development',
-//     devtool: 'source-map',
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.html$/,
-//                 // loader: 'raw-loader',
-//                 use: [
-//                     {
-//                         loader: 'html-loader',
-//                         options: {
-//                             minimize: false,
-//                             attributes: {
-//                                 list: [
-//                                     {
-//                                         tag: 'img',
-//                                         attribute: 'srcset',
-//                                         type: 'src',
-//                                     },
-//                                     {
-//                                         tag: 'img',
-//                                         attribute: 'src',
-//                                         type: 'src',
-//                                     },
-//                                 ],
-//                             },
-//                         },
-//                     },
-//                 ],
-//             },
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env', '@babel/preset-react'],
-//                     },
-//                 },
-//             },
-//             {
-//                 test: /\.jsx?$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env', '@babel/preset-react'],
-//                     },
-//                 },
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     MiniCssExtractPlugin.loader,
-//                     'css-loader',
-//                     'postcss-loader',
-//                 ],
-//             },
-//             {
-//                 test: /\.module\.css$/,
-//                 use: [
-//                     MiniCssExtractPlugin.loader,
-//                     {
-//                         loader: 'css-loader',
-//                         options: {
-//                             modules: true,
-//                         },
-//                     },
-//                     'postcss-loader',
-//                 ],
-//             },
-//             {
-//                 test: /\.scss$/,
-//                 use: [
-//                     MiniCssExtractPlugin.loader,
-//                     'css-loader',
-//                     'postcss-loader',
-//                     'sass-loader',
-//                 ],
-//             },
-//         ],
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: './src/index.html',
-//             inject: 'body',
-//         }),
-//         new MiniCssExtractPlugin({
-//             filename: 'styles.css',
-//         }),
-//         new ImageminWebpWebpackPlugin({
-//             config: [{
-//                 test: /\.(jpe?g|png)$/i,
-//                 options: {
-//                     quality: 75,
-//                 },
-//             }],
-//             overrideExtension: true,
-//         }),
-//         new CopyWebpackPlugin({
-//             patterns: [
-//                 { from: 'src/images', to: 'images' },
-//             ],
-//         }),
-//         new CleanWebpackPlugin(), // Доданий CleanWebpackPlugin
-//     ],
-// };
